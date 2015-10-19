a35213e356fb8fc4630521a7e8cfdc952166aa32
Sarah Hicks <sarah.hicks@emerson.com>
2015-10-19 13:37:59 -0500

    Refactored per assignment 3.2.3

--- app.js
+++ app.js
@@ -6,12 +6,57 @@
 
 /*The program should print out each number (not 'one' for 1, but 1 for 1), from 1 to 100, replacing numbers divisible by both 3 and 5 with “fizz buzz”, those divisible by 3 with “fizz”, and those divisible by 5 with “buzz”.*/
 
+//REFACTORING REQUIREMENTS
+//When the page loads, the user should be prompted to supply a number. The
+// easiest way to do this would be with the prompt() function, but you should
+// also feel free to create a simple text input with text telling the user to
+// input a number.
+
+//You’ll need to write one named function that takes an integer as argument,
+// and then counts from 1 to the argument value, substituting “fizz”, “buzz”,
+// and “fizzbuzz” accordingly.
+
+//You’ll need to convert the value the user supplies from a string to a number.
+// Remember that the value that you get from prompt() or the val() on your form
+// will by default be a string. To convert this to an integer you can use the +
+// operator to convert a string to an integer. For instance, if you had the
+// string “22”, you could convert that to a number with +”22”.
+
+//You will need to validate user input to ensure the user is submitting a
+// number. You can use the parseInt() function in javascript to convert a
+// string into an integer. For example, parseInt('597a') will return 597 where
+// parseInt('hello') will return NaN. Read more about parseInt() on MDN.
+
+//Optionally, you can also write code to ensure that the user has not supplied
+// a decimal value. For an easy way to do this, check out this answer on Stack
+// Overflow.
+
+//DO NOT spend any time thinking about design for this app. Like the first
+// version, the goal here is not to create a pretty app, but instead to hone
+// your JavaScript skills.
+
+//As usual, use Git to store your changes. When you’ve completed this
+// assignment, push it up to GitHub pages, and share a link with your mentor
+// and fellow students.
+
+
+
 $( document ).ready(function() {
+    //supply a number
+    userValue = window.prompt("Enter an integer larger than 10");
+
+    fizzbuzz(userValue);
+
     var result = "begin";
     $('li:last-child').text(result);
+});
 
-    for (var i = 1; i < 101; i++) {
+function fizzbuzz(aString){
+//counts from 1 to the argument value, substituting “fizz”, “buzz”,
+// and “fizzbuzz” accordingly
+    var myInt = parseInt(aString);
 
+    for (var i = 0; i < myInt; i++){
         //is i evenly divisible by 3? result = "fizz"
         if (i % 3 === 0 && i % 5 === 0) {
             result = "FizzBuzz";
@@ -39,4 +84,4 @@ $( document ).ready(function() {
             $('#list').append("<li>" + result + "</li>");
         }
     }
-});
+}
